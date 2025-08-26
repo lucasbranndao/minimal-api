ab780781bba7fa48f0062bd8994ea70c620925be
Lucas Brandão <lucasbranndao@gmail.com>
2025-08-26 16:29:44 -0300

    add required

--- Dominio/Emtidades/Administrador.cs
+++ Dominio/Emtidades/Administrador.cs
@@ -11,6 +11,6 @@ public class Administrador
     public int Id { get; set; } = default!;
 
     [Required][StringLength(255)] public string Email { get; set; } = default!;
-    [StringLength(50)] public string Senha { get; set; } = default!;
-    [StringLength(10)] public string Perfil { get; set; } = default!;
+    [Required][StringLength(50)] public string Senha { get; set; } = default!;
+    [Required][StringLength(10)] public string Perfil { get; set; } = default!;
 }
