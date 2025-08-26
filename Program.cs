61c13a90348b96daca03d83c2b884feeac051131
Lucas Brandão <lucasbranndao@gmail.com>
2025-08-26 15:10:29 -0300

    criando DB, conexao, tabela e user adm

--- Program.cs
+++ Program.cs
@@ -1,8 +1,20 @@
+using MinimalApi.Infraestrutura.Db;
+using MinimalApi.DTOs;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<DbContexto>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+            );
+});
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-app.MapPost("/login", (MinimalApi.DTOs.LoginDTO loginDTO) =>
+app.MapPost("/login", (LoginDTO loginDTO) =>
 {
     if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
 
